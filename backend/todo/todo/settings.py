795c8b0809660b42c3f2fd2c1bd111afcb98057b
Ranga19 <gaddamranganath32@gmail.com>
2024-02-04 09:25:15 +0530

    added Tasks app

--- backend/todo/todo/settings.py
+++ backend/todo/todo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'corsheaders',
+    'Tasks',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",  # Example: React development server
+    # "https://yourfrontenddomain.com",  # Add your frontend domain
+]
